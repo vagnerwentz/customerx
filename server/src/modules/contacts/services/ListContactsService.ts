dabb2b8d40632e02c121a9c7582e2f92db75b66d
vagnerwentz <wentz.vagner@gmail.com>
2020-08-17 03:56:08 -0300

    feat: server

--- server/src/modules/contacts/services/ListContactsService.ts
+++ server/src/modules/contacts/services/ListContactsService.ts
@@ -1,6 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
 import Contact from '../infra/typeorm/entities/Contact';
 import IContactsRepository from '../repositories/IContactsRepository';
 
@@ -13,10 +12,6 @@ class ListContactsService {
 
   async execute(): Promise<Contact[]> {
     const contacts = await this.contactsRepository.listContact();
-
-    if (contacts.length === 0) {
-      throw new AppError('The contact is empty', 204);
-    }
 
     return contacts;
   }
